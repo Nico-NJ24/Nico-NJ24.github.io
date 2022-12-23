15b98a141fd0725a7fadd146158a8584ad581aaa
Nico-NJ24 <82044241+Nico-NJ24@users.noreply.github.com>
2022-12-24 00:43:48 +0100

    Update defines.php

--- defines.php
+++ defines.php
@@ -1,5 +1,5 @@
 <?php
 define( 'INSTAGRAM_APP_ID', '1278600299539632' );
 define( 'INSTAGRAM_APP_SECRET', 'e4768491dba2b6e82ad6482d16a7a765' );
-define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://nj24.jimdosite.com' );
+define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://nico-nj24.github.io/' );
 
